4ec1f4b6264faa4c892a7d75a7ceb15f7a9ec29a
jarababalo <baballojara@gmail.com>
2022-07-04 16:23:38 -0700

    printf commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * return: always 0 (success)
+ * return: Always 0 (success)
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- *return: always 0 (success)
+ *return: Always 0 (success)
  */
 int main(void)
 {
